ad108b9379d04135ef2bbd3f239ecdfa5d813197
LamarC <dviann89@gmail.com>
2019-01-22 15:05:09 -0600

    Server created

--- google-books-search/server.js
+++ google-books-search/server.js
@@ -1,24 +1,25 @@
 const express = require("express");
-const path = require("path");
-const PORT = process.env.PORT || 3001;
+const routes = require("./routes");
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-// Define middleware here
+//Middleware 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+app.use(express.json);
+
+if(process.env.NODE_EVN === "production") {
+    app.use(express.static("client/build"));
 }
 
-// Define API routes here
+//Routes
+app.use(routes);
 
-// Send every other request to the React app
-// Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+//Connect to Mongo DB
+const moongoose = require("mongoose");
+moongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist"
+);
 
-app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
-});
+app.listen(PORT, function() {
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+  });
